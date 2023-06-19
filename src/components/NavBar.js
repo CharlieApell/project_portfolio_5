29802a811ff712d2e655061feeb2dd254745e300
CharlieApell <charlie.apell@gmail.com>
2023-06-19 17:50:19 +0000

    styling sign in and sign up pages and adding warning messages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/logo.png';
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
@@ -35,7 +35,7 @@ const NavBar = () => {
                 <NavLink
                 className={styles.NavLink}
                 activeClassName={styles.Active}
-                to ="/register">
+                to ="/signup">
                 <i className='fas fa-user-plus'></i>Sign up
                 </NavLink>
 
