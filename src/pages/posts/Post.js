953612dc649e27ab5ed149cdf0e5a02b67f34a66
CharlieApell <charlie.apell@gmail.com>
2023-06-22 16:07:15 +0000

    added dropdown menu to edit and delete posts, and a pop-up warning with the text Are you sure you want to delete this item?

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
+import btnStyles from "../../styles/Button.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
+import { MoreDropdown } from "../../components/MoreDropdown";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Post = (props) => {
   const {
@@ -25,6 +28,30 @@ const Post = (props) => {
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+
+  const handleEdit = () => {
+    history.push (`/posts/${id}/edit`)
+  };
+
+  const [showDeletePopup, setShowDeletePopup] = React.useState(false);
+
+  const handleDelete = (item) => {
+    setShowDeletePopup(true);
+  };
+
+  const handleDeleteConfirmed = async () => {
+    try {
+      await axiosRes.delete(`/posts/${id}/`);
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  const handleDeleteCancelled = () => {
+    setShowDeletePopup(false);
+  };
 
   const handleLike =async () => {
     try {
@@ -69,9 +96,23 @@ const Post = (props) => {
           </Link>
           <div className="d-flex align-items-center">
             <span>{updated_at}</span>
-            {is_owner && postPage && "..."}
+            {is_owner && postPage && (
+            <MoreDropdown
+            handleEdit={handleEdit}
+            handleDelete={handleDelete} />
+            )}
           </div>
         </Media>
+        {showDeletePopup && (
+          <div className={styles.DeletePopup}>
+            <p>Are you sure you want to delete this item?</p>
+            <div className={styles.DeletePopupButtons}>
+              <button className={`${btnStyles.Button} ${btnStyles.Blue}`} onClick={handleDeleteConfirmed}>Yes</button>
+              <button className={`${btnStyles.Button} ${btnStyles.Blue}`} onClick={handleDeleteCancelled}>No</button>
+            </div>
+          </div>
+        )}
+        
       </Card.Body>
       <Link to={`/posts/${id}`}>
         <Card.Img src={image} alt={title} />
